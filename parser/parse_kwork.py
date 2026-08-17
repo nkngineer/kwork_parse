0758f5d064cf224149312ec810b9d1c50b5b2c64
flyofk <ggnekitos@gmail.com>
2026-08-17 13:11:07 +0300

    Remove .env from tracking

--- parser/parse_kwork.py
+++ parser/parse_kwork.py
@@ -6,51 +6,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from mistral import get_categorized_description
-from parse_json import decode_json_list
+from gigachat_api import get_categorized_description
 
 """
 TODO:
-1. парсинг биржи
-    - добавить фильтрацию (✅️, сделано через фильтры при логине. Был вариант вручную постоянно выбирать фильтры, но пусть пока будет в долгом ящике)
-    - разделить название, цену, дату, подробное описание на разные части (✅️)
-    - добавить парсинг всех страниц по фильтрам (✅️)
-2. ИИ(mistral) обрабатывает описание и делает краткую сводку(что делать, какие инструменты и навыки требуются)
-    - создать качественный промпт
-    - подключить Mistral по API ✅️
-    - протестировать промпт на Mistral ✅️
-    - скормить карточки на каждой странице ИИ в формате json
-        - сделать 2 варианта
-            - парсинг и закидывание в mistral каждой карточки отдельно
-            - парсинг и закидывание в mistral пачку карточек средней длины(допустим 5-6 карточек)
-    - Структуризировать __init__
 3. отправка в тг бота
     - сделать тг бота с вебхуками, который при обновлении ленты биржи будет высылать новые заказы в тг
-4. автообновление страницы +добавление новых заказов
+4. автообновление страницы + добавление новых заказов
 5. сохранение истории(?)
-"""
-
-"""
-https://developers.sber.ru/docs/ru/gigachat/guides/selecting-a-model?lang=py
 """
 
 
 class Connection:
-    def __init__(self, login: str, password: str, profile_path: str):
+    def __init__(self, login: str, password: str, profile_path: str) -> None:
         """
-        Структуризировать __init__
+        Инициализация:
+        - Передача логина и пароля от kwork.ru, а также путь к конфигу с данными(чтобы не логиниться каждый раз)
+        - Создание необходимых для работы переменных
         """
         self.login: str = login
         self.password: str = password
-        options = webdriver.ChromeOptions()
         self.profile_path: str = profile_path
-        options.add_argument(f"--user-data-dir={self.profile_path}")
-        options.add_argument("--profile-directory=Default")
-        self.driver = webdriver.Chrome(options=options)
+
+        self.options = webdriver.ChromeOptions()
+        self.driver = webdriver.Chrome(options=self.options)
+        self.selenium_set()
+
         self.page_len: int = 0
-        self.descriptions_list: list[str] = []
-        self.descriptions_str: str = ""
         self.get_page_len()
+
+    def selenium_set(self):
+        self.options.add_argument(f"--user-data-dir={self.profile_path}")
+        self.options.add_argument("--profile-directory=Default")
 
     def auth(self):
         self.driver.get("https://kwork.ru/login")
@@ -82,8 +69,8 @@ class Connection:
 
     def get_page_len(self):
         """
-        Либо оставить, но держать в голове, что страниц всегда на одну меньше
-        Либо как-то проверять что содержит этот класс, цифру, или стрелку перемещения страниц
+        Страниц всегда на одну меньше
+        (из-за того, что в парсинге учитывается стрелка переключения на следующую страницу)
         """
         self.driver.get("https://kwork.ru/projects")
         pages = self.driver.find_elements(By.CLASS_NAME, "pagination__item")
@@ -91,7 +78,8 @@ class Connection:
         self.page_len = len(pages)
 
     def parse_marketplace(self):
-        for page in range(1, len(pages)):
+
+        for page in range(1, self.page_len):
             url = f"https://kwork.ru/projects?a=1&page={page}"
             self.driver.get(url)
             cards = self.driver.find_elements(By.CLASS_NAME, "want-card")
@@ -105,7 +93,7 @@ class Connection:
                 )
                 self.driver.execute_script("arguments[0].click();", more_button)
 
-                self.descriptions_list.append(card_description.text)
+                ai_decription = get_categorized_description(card_description.text)
 
                 card_cost = card.find_element(By.CLASS_NAME, "wants-card__right")
 
@@ -119,19 +107,8 @@ class Connection:
                 print(card_description.text)
                 print(card_cost.text)
                 print(time_left_element.text)
-
-        self.format_for_json()
-        cat_desc = get_categorized_description(self.descriptions_str)
-        res = decode_json_list(cat_desc)
-        print(res)
-
-    def format_for_json(self):
-        for card_id, description_card in enumerate(self.descriptions_list, start=1):
-            self.descriptions_str += (
-                f"Card_id : {card_id}\nCard_description : {description_card}\n"
-            )
-
-        print(self.descriptions_str)
+                print("---")
+                print(ai_decription)
 
 
 def main():
--- test.md
+++
@@ -1,4 +0,0 @@
-1. dasdsf
-2. dasdsf
-3. dasdsf
-4. dasdsf
